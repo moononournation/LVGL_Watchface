609ba1eac17540b9055d1d59d5be4c24a6f185d7
moononournation <moononournation@gmail.com>
2023-02-05 23:43:39 +0800

    add animated GIF example

---
+++ LVGL_Watchface_240_Stormtrooper/LVGL_Watchface_240_Stormtrooper.ino
@@ -0,0 +1,175 @@
+/*******************************************************************************
+ * LVGL Watchface
+ *
+ * GIF animation from:
+ * https://giphy.com/gifs/illustration-vector-xTiTnJK44NXT46iKYM
+ * 
+ * Image from:
+ * https://en.m.wikipedia.org/wiki/File:Dueling_lightsabers.svg
+ * https://battlefront.fandom.com/wiki/DLT-19X
+ * https://www.onlinewebfonts.com/icon/555311
+ *
+ * Dependent libraries:
+ * LVGL: https://github.com/lvgl/lvgl.git
+ *
+ * LVGL Configuration file:
+ * Copy your_arduino_path/libraries/lvgl/lv_conf_template.h
+ * to your_arduino_path/libraries/lv_conf.h
+ *
+ * In lv_conf.h around line 15, enable config file:
+ * #if 1 // Set it to "1" to enable content
+ *
+ * Then find and set:
+ * #define LV_COLOR_DEPTH     16
+ * #define LV_TICK_CUSTOM     1
+ *
+ * For SPI/parallel 8 display set color swap can be faster, parallel 16/RGB screen don't swap!
+ * #define LV_COLOR_16_SWAP   1 // for SPI and parallel 8
+ * #define LV_COLOR_16_SWAP   0 // for parallel 16 and RGB
+ *
+ * Enable GIF decoder library
+ * #define LV_USE_GIF 1
+ ******************************************************************************/
+#include <lvgl.h>
+#include "ui.h"
+
+/*******************************************************************************
+ * Start of Arduino_GFX setting
+ ******************************************************************************/
+#include <Arduino_GFX_Library.h>
+#define GFX_BL 25
+/* More data bus class: https://github.com/moononournation/Arduino_GFX/wiki/Data-Bus-Class */
+Arduino_DataBus *bus = new Arduino_RPiPicoSPI(8 /* DC */, 9 /* CS */, 10 /* SCK */, 11 /* MOSI */, 12 /* MISO */, spi1 /* spi */);
+/* More display class: https://github.com/moononournation/Arduino_GFX/wiki/Display-Class */
+Arduino_GFX *gfx = new Arduino_GC9A01(bus, 12, 0 /* rotation */, true /* IPS */);
+/*******************************************************************************
+ * End of Arduino_GFX setting
+ ******************************************************************************/
+
+// use compile time for demo only
+#define ONE_MINUTE_MS (60 * 1000)
+#define ONE_HOUR_MS (60 * 60 * 1000)
+#define TWELVE_HOUR_MS (12 * 60 * 60 * 1000)
+static uint8_t conv2d(const char *p)
+{
+  uint8_t v = 0;
+  return (10 * (*p - '0')) + (*++p - '0');
+}
+static unsigned long ms_offset;
+
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+
+void my_log_cb(const char * buf)
+{
+  Serial.println(buf);
+}
+
+void setup()
+{
+  // use compile time for demo only
+  uint8_t hh = conv2d(__TIME__);
+  uint8_t mm = conv2d(__TIME__ + 3);
+  uint8_t ss = conv2d(__TIME__ + 6);
+  ms_offset = ((60 * 60 * hh) + (60 * mm) + ss) * 1000;
+
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+  // while (!Serial);
+  Serial.println("LVGL Watchface");
+
+#ifdef GFX_EXTRA_PRE_INIT
+  GFX_EXTRA_PRE_INIT();
+#endif
+
+  // Init Display
+  gfx->begin(100000000);
+  gfx->fillScreen(BLACK);
+
+#ifdef GFX_BL
+  pinMode(GFX_BL, OUTPUT);
+  // digitalWrite(GFX_BL, HIGH);
+  analogWrite(GFX_BL, 127);
+#endif
+
+#if LV_USE_LOG
+  lv_log_register_print_cb(my_log_cb);
+#endif
+
+  lv_init();
+
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
+#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * 32, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * 32);
+#endif
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * 32);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    lv_indev_drv_register(&indev_drv);
+
+    /* Init SquareLine prepared UI */
+    ui_init();
+
+    // In ui.c, replace "ui_ImageGIF = lv_gif_create(ui_Screen1);" to "ui_ImageGIF = lv_gif_create(ui_Screen1);"
+    LV_IMG_DECLARE(star_wars_stormtrooper_48_gif);
+    lv_gif_set_src(ui_ImageGIF, &star_wars_stormtrooper_48_gif);
+
+    Serial.println("Setup done");
+  }
+}
+
+void loop()
+{
+  lv_timer_handler(); /* let the GUI do its work */
+
+  unsigned long ms = millis();
+
+  unsigned long clock_ms = (ms_offset + ms) % TWELVE_HOUR_MS;
+  uint8_t hour = clock_ms / ONE_HOUR_MS;
+  uint8_t minute = (clock_ms % ONE_HOUR_MS) / ONE_MINUTE_MS;
+  int16_t angle = (clock_ms % ONE_MINUTE_MS) * 3600 / ONE_MINUTE_MS;
+  lv_img_set_angle(ui_ImageArmSecond, angle);
+  angle = (angle + (minute * 3600)) / 60;
+  lv_img_set_angle(ui_ImageArmMinute, angle);
+  angle = (angle + (hour * 3600)) / 12;
+  lv_img_set_angle(ui_ImageArmHour, angle);
+}
---
+++ LVGL_Watchface_240_Stormtrooper/ui.c
@@ -0,0 +1,97 @@
+// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.2.0
+// LVGL VERSION: 8.2.0
+// PROJECT: SquareLine_Project
+
+#include "ui.h"
+#include "ui_helpers.h"
+
+///////////////////// VARIABLES ////////////////////
+lv_obj_t *ui_Screen1;
+lv_obj_t *ui_ImageWatchface;
+lv_obj_t *ui_ImageGIF;
+lv_obj_t *ui_ImageArmHour;
+lv_obj_t *ui_ImageArmMinute;
+lv_obj_t *ui_ImageArmSecond;
+
+///////////////////// TEST LVGL SETTINGS ////////////////////
+#if LV_COLOR_DEPTH != 16
+    #error "LV_COLOR_DEPTH should be 16bit to match SquareLine Studio's settings"
+#endif
+#if LV_COLOR_16_SWAP !=1
+    #error "LV_COLOR_16_SWAP should be 1 to match SquareLine Studio's settings"
+#endif
+
+///////////////////// ANIMATIONS ////////////////////
+
+///////////////////// FUNCTIONS ////////////////////
+
+///////////////////// SCREENS ////////////////////
+void ui_Screen1_screen_init(void)
+{
+ui_Screen1 = lv_obj_create(NULL);
+lv_obj_clear_flag( ui_Screen1, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+
+ui_ImageWatchface = lv_img_create(ui_Screen1);
+lv_img_set_src(ui_ImageWatchface, &ui_img_watchface240_png);
+lv_obj_set_width( ui_ImageWatchface, LV_SIZE_CONTENT);  /// 1
+lv_obj_set_height( ui_ImageWatchface, LV_SIZE_CONTENT);   /// 1
+lv_obj_set_align( ui_ImageWatchface, LV_ALIGN_CENTER );
+lv_obj_add_flag( ui_ImageWatchface, LV_OBJ_FLAG_ADV_HITTEST );   /// Flags
+lv_obj_clear_flag( ui_ImageWatchface, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+
+ui_ImageGIF = lv_gif_create(ui_Screen1);
+lv_obj_set_width( ui_ImageGIF, LV_SIZE_CONTENT);  /// 1
+lv_obj_set_height( ui_ImageGIF, LV_SIZE_CONTENT);   /// 1
+lv_obj_set_x( ui_ImageGIF, 0 );
+lv_obj_set_y( ui_ImageGIF, 56 );
+lv_obj_set_align( ui_ImageGIF, LV_ALIGN_CENTER );
+lv_obj_add_flag( ui_ImageGIF, LV_OBJ_FLAG_ADV_HITTEST );   /// Flags
+lv_obj_clear_flag( ui_ImageGIF, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+
+ui_ImageArmHour = lv_img_create(ui_Screen1);
+lv_img_set_src(ui_ImageArmHour, &ui_img_blaster_png);
+lv_obj_set_width( ui_ImageArmHour, LV_SIZE_CONTENT);  /// 1
+lv_obj_set_height( ui_ImageArmHour, LV_SIZE_CONTENT);   /// 1
+lv_obj_set_x( ui_ImageArmHour, -4 );
+lv_obj_set_y( ui_ImageArmHour, -26 );
+lv_obj_set_align( ui_ImageArmHour, LV_ALIGN_CENTER );
+lv_obj_add_flag( ui_ImageArmHour, LV_OBJ_FLAG_ADV_HITTEST );   /// Flags
+lv_obj_clear_flag( ui_ImageArmHour, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+lv_img_set_pivot(ui_ImageArmHour,17,56);
+lv_img_set_angle(ui_ImageArmHour,750);
+
+ui_ImageArmMinute = lv_img_create(ui_Screen1);
+lv_img_set_src(ui_ImageArmMinute, &ui_img_dlt19x_png);
+lv_obj_set_width( ui_ImageArmMinute, LV_SIZE_CONTENT);  /// 1
+lv_obj_set_height( ui_ImageArmMinute, LV_SIZE_CONTENT);   /// 1
+lv_obj_set_x( ui_ImageArmMinute, 1 );
+lv_obj_set_y( ui_ImageArmMinute, -45 );
+lv_obj_set_align( ui_ImageArmMinute, LV_ALIGN_CENTER );
+lv_obj_add_flag( ui_ImageArmMinute, LV_OBJ_FLAG_ADV_HITTEST );   /// Flags
+lv_obj_clear_flag( ui_ImageArmMinute, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+lv_img_set_pivot(ui_ImageArmMinute,11,105);
+lv_img_set_angle(ui_ImageArmMinute,1800);
+
+ui_ImageArmSecond = lv_img_create(ui_Screen1);
+lv_img_set_src(ui_ImageArmSecond, &ui_img_lightsaber_png);
+lv_obj_set_width( ui_ImageArmSecond, LV_SIZE_CONTENT);  /// 1
+lv_obj_set_height( ui_ImageArmSecond, LV_SIZE_CONTENT);   /// 1
+lv_obj_set_x( ui_ImageArmSecond, -1 );
+lv_obj_set_y( ui_ImageArmSecond, -50 );
+lv_obj_set_align( ui_ImageArmSecond, LV_ALIGN_CENTER );
+lv_obj_add_flag( ui_ImageArmSecond, LV_OBJ_FLAG_ADV_HITTEST );   /// Flags
+lv_obj_clear_flag( ui_ImageArmSecond, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
+lv_img_set_pivot(ui_ImageArmSecond,8,117);
+lv_img_set_angle(ui_ImageArmSecond,3150);
+
+}
+
+void ui_init( void )
+{
+lv_disp_t *dispp = lv_disp_get_default();
+lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
+lv_disp_set_theme(dispp, theme);
+ui_Screen1_screen_init();
+lv_disp_load_scr( ui_Screen1);
+}
